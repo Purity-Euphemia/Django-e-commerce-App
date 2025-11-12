1096f1832a68883a4d5822b1f76eb70d5c456bc8
Purity-Euphemia <euphemiaobachie@gmail.com>
2025-11-12 12:04:00 +0100

    first commit

--- store/models.py
+++ store/models.py
@@ -6,4 +6,26 @@ class Product(models.Model):
     description = models.TextField()
     price = models.DecimalField(decimal_places=2, max_digits=6)
     updated_at = models.DateTimeField(auto_now=True)
-    inventory = models.IntegerField(default=0)
+    inventory = models.PositiveIntegerField()
+
+class Collection(models.Model):
+    title = models.CharField(max_length=100)
+
+class Cart(models.Model):
+    created_at = models.DateTimeField(auto_now_add=True)
+
+class CartItem(models.Model):
+    quantity = models.PositiveIntegerField()
+
+class Order(models.Model):
+    ORDER_STATUS = (
+        ('P', 'Pending'),
+        ('C', 'Completed'),
+        ('F', 'Filled'),
+    )
+    placed_at = models.DateTimeField(auto_now_add=True)
+    status = models.CharField(max_length=1, choices=ORDER_STATUS, default='P')
+
+class OrderItem(models.Model):
+    quantity = models.PositiveIntegerField()
+    unit_price = models.DecimalField(decimal_places=2, max_digits=6)
